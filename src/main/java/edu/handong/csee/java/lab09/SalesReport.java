01391d5d26b88daf214eaadcb400b7b24ce2d1f1
KimSukJin <21700105@handong.edu>
2018-04-14 22:13:29 +0900

    Use ArrayList

--- src/main/java/edu/handong/csee/java/lab09/SalesReport.java
+++ src/main/java/edu/handong/csee/java/lab09/SalesReport.java
@@ -1,84 +1,91 @@
 package edu.handong.csee.java.lab09;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 /**
- * This class gets salesmen' informations and shows us guy who is the best seller.
- * 
- * @since 180412
+ * This class gets some information of salesmen and print out who is the best seller.
  * @author KimSukJin
- *
+ * @since 180414
  */
 public class SalesReport {
 
-	private double highestSales; //instantiating variables.
-	private double averageSales; //instantiating variables.
-	private Salesman[] team; //instantiating Array
-	private int numOfSalesman; //instantiating variables.
-
-	public SalesReport() {
-		System.out.println("Enter number of Salesman:");
-		Scanner myScanner = new Scanner(System.in);
-		numOfSalesman = myScanner.nextInt();
-	} // gets number from user and puts into variable 'numOfSalesman'
+	private double highestSales; //instantiates variables
+	private double averageSales; //instantiates variables
+	ArrayList<String> members = new ArrayList<String>(); //instantiates ArrayList
+	static ArrayList<Double> amountOfSales = new ArrayList<Double>(); //instantiates ArrayList
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
 		SalesReport myReporter = new SalesReport(); 
-		myReporter.getData();
-		myReporter.calculateAverageSales();
-		myReporter.highestSales();
+		myReporter.run();
 		myReporter.printOutResults();
 	}
+/**
+ * This method gets information from user and calculate average sales and the highest sales 
+ * 
+ */
+	public void run() {
+		System.out.println("Enter some information of salesmen");
 
-	public void getData() {
-		team = new Salesman[numOfSalesman]; 
+		Scanner myScanner = new Scanner(System.in); 
 
-		for (int i = 0; i < numOfSalesman; i++) { //repeats until NumOfSalesman
-			Scanner myScanner = new Scanner(System.in); 
-
+		boolean done = false;
+		
+		while (!done) {
 			System.out.println("Enter name: ");
-			String name = myScanner.nextLine(); // gets salesman's name
-			
+			String name = myScanner.nextLine(); 
+
 			System.out.println("Enter sales: ");
-			double sales = myScanner.nextDouble(); //gets salesman's sales
+			double sales = myScanner.nextDouble();
+
+			members.add(name); //substitutes name into ArrayList
+			amountOfSales.add(sales); //substitutes sales into ArrayList
+
+			System.out.println("More members for the list? (Enter \"yes\" if you want)");
 			
-			Salesman mySalesman = new Salesman();
-			mySalesman.setmName(name);
-			mySalesman.setSales(sales);
-			team[i] = mySalesman; //inputs mySalesman into team array
+			myScanner.nextLine();
+			String ans = myScanner.nextLine();
+			
+			if (!ans.equalsIgnoreCase("yes"))
+				done = true; //User enter 'yes', then quit
 		}
-	}
 
-	public void calculateAverageSales() {
-		double sum = 0; //instantiating variables.
+		int listsize = amountOfSales.size();
+		double sum = 0;
+		for (int index = 0; index < listsize; index++) {
+			sum = sum + amountOfSales.get(index);
+		} //adds every value in ArrayList
+		averageSales = sum / listsize; //calculates average
 
-		for (int i = 0; i < team.length; i++) { //repeats until length of team
-			double sales = team[i].getSales();
-			sum = sum + sales; //adds all sales
-		} 
-
-		averageSales = sum / team.length; //gets average
-	}
-
-	public void highestSales() {
-		for (int i = 0; i < team.length - 1; i++) {//reapeat until length of team - 1
-			if (team[i].getSales() < team[i + 1].getSales()) { //if data in low index number is smaller than high index number 
-				highestSales = team[i + 1].getSales(); // inputs sales that is highest into highestSales
-			}
+		listsize = amountOfSales.size();
+		if (listsize == 1)
+			highestSales = amountOfSales.get(0); //If ArrayList has one index, that value is the highest
+		else {
+			for (int index = 0; index < listsize - 1 ; index++) {
+				highestSales = amountOfSales.get(0);
+				if(highestSales < amountOfSales.get(index + 1))
+					highestSales = amountOfSales.get(index + 1);
+							}// calculates value that is the highest
 		}
+		myScanner.close();
 	}
-
+/**
+ * This method prints out results that shows average of ArrayList and man who is the best seller.
+ */
 	public void printOutResults() {
-		System.out.println("Average: " + averageSales);
-		System.out.println("Highest sales: " + highestSales); //print out average and highest sales
+
+		System.out.println("Average: " + averageSales); //prints out average
+		System.out.println("Highest sales: " + highestSales); //prints out highest sales
 
 		System.out.println("The Highest sales guy");
-		for (int i = 0; i < team.length; i++) { //repeats until length of team
-			if (team[i].getSales() == highestSales) { 
-				System.out.println("Name: " + team[i].getmName());
-				System.out.println("Sales: " + team[i].getSales());
-			}
-		} //print out information of guy who is the best seller
+
+		int listsize = amountOfSales.size();
+		for (int position = 0; position < listsize; position++) {
+			if (amountOfSales.get(position) == highestSales) {
+				System.out.println("Name: " + members.get(position));
+				System.out.println("Sales: " + amountOfSales.get(position));
+			} //prints out informaion of the best seller
+		}
 	}
 
 }
